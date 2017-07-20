0db29897eb395387329375b7dfc1d6b190dad87e
dudulaopo833 <1294318974@qq.com>
2017-07-20 23:52:45 +0800

    plugins with filename and template

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports ={
 	entry: {app: "./src/app.js"},
 	output: {
 		path: __dirname + "/dist",
 		filename: "[name]-[hash].js" //entry should be object, then can use [name]
-	}		
+	},
+	plugins:[
+		new htmlWebpackPlugin({
+			template: "index.html",
+			filename: "index-[hash].html"
+		})
+	]
+		
 }
